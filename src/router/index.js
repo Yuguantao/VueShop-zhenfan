6e4a3feabefca6b2c760285431abe73a5b734584
Yuguantao <Guantaoyu@yeah.net>
2017-08-17 21:38:52 +0800

    add 20170817

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import GoodsList from '@/view/GoodsList'
+import Cart from '@/view/Cart'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: GoodsList
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: Cart
+    }
+  ]
+})
